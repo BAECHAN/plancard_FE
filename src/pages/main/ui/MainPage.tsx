d1a8023b391bb6d7bf04f375ce18ee5479399007
baechan <asdfjklddd@naver.com>
2024-11-03 14:28:13 +0900

    deploy: Github Actions 테스트

--- src/pages/main/ui/MainPage.tsx
+++ src/pages/main/ui/MainPage.tsx
@@ -3,7 +3,7 @@ import MainLayout from '@/widgets/layout/ui/MainLayout';
 export const MainPage = () => {
   return (
     <MainLayout>
-      <div className="h-[1000px]">그러지말자</div>
+      <div className="h-[1000px]">이게 되네</div>
     </MainLayout>
   );
 };
