ea11610a6ea661854a734fab2096fddf7d17b3b2
baechan <asdfjklddd@naver.com>
2024-11-03 12:14:37 +0900

    Updates

---
+++ .storybook/preview.ts
@@ -0,0 +1,18 @@
+import '@/app/index.css';
+import '@fontsource/noto-sans-kr/400.css';
+import '@fontsource/noto-sans-kr/700.css';
+import type { Preview } from '@storybook/react';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
