839ccf31d153f0035afcb10221d502426c3e0844
baechan <asdfjklddd@naver.com>
2024-11-28 20:30:57 +0900

    directory: FocusableSearchInputText 위치를 Widget에서 Shared로 변경

---
+++ src/stories/search/FocusableSearchInputText.stories.tsx
@@ -0,0 +1,45 @@
+import { FocusableSearchInputText } from '@/shared/ui';
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+const meta: Meta<typeof FocusableSearchInputText> = {
+  title: 'search/FocusableSearchInputText',
+  component: FocusableSearchInputText,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    initialValue: {
+      control: 'text',
+      description: '초기값 텍스트',
+    },
+
+    placeholder: {
+      control: 'text',
+      description: '검색 텍스트 플레이스홀더',
+    },
+    label: {
+      control: 'text',
+      description: '검색 텍스트 라벨',
+    },
+    type: {
+      control: 'text',
+      description: '검색 텍스트 타입',
+    },
+  },
+  args: {
+    onSearch: fn(),
+    type: 'text',
+    placeholder: '검색어를 입력하세요',
+  },
+} satisfies Meta<typeof FocusableSearchInputText>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    initialValue: '감사',
+  },
+};
--- src/widgets/search/ui/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as FocusableSearchInputText } from './FocusableSearchInputText';
