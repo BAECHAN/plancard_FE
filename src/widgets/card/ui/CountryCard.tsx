4549f750c6a48a38eff5316fdb76e4c92e50b6c1
baechan <asdfjklddd@naver.com>
2024-12-02 20:11:14 +0900

    style: border-m 제거

--- src/widgets/card/ui/CountryCard.tsx
+++ src/widgets/card/ui/CountryCard.tsx
@@ -22,7 +22,7 @@ const CountryCard = ({ onClick, info, size = 'medium' }: CountryCardProps) => {
 
   return (
     <div
-      className={`rounded-md ${sizeClass[size]} flex flex-col gap-1 relative text-ellipsis whitespace-pre-wrap border border-m border-[#D9D9DE] `}
+      className={`rounded-md ${sizeClass[size]} flex flex-col gap-1 relative text-ellipsis whitespace-pre-wrap border border-[#D9D9DE] `}
       onClick={onClick}
     >
       <img
