47448f44f3bcc283167598aa6476dd79dbc37b4f
baechandorosee@gmail.com <baechandorosee@gmail.com>
2024-12-16 14:15:23 +0900

    feat: useInput 훅에 reset과 ref추가

--- src/shared/hooks/useInput.tsx
+++ src/shared/hooks/useInput.tsx
@@ -1,13 +1,39 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 const useInput = (initialValue: string = '') => {
   const [value, setValue] = useState<string>(initialValue);
+  const ref = useRef<HTMLInputElement | null>(null);
 
   const onChange = useCallback((updatedValue: string) => {
     setValue(updatedValue);
   }, []);
 
-  return { value, onChange };
+  const onReset = useCallback(() => {
+    onChange('');
+  }, [onChange]);
+
+  useEffect(
+    function attachESCKeydownEventListener() {
+      const handleResetESCKeyDown = (event: KeyboardEvent) => {
+        if (event.key === 'Escape') {
+          onReset();
+        }
+      };
+
+      const target = ref.current;
+      if (!value || !target) {
+        return;
+      }
+
+      target.addEventListener('keydown', handleResetESCKeyDown);
+      return () => {
+        target.removeEventListener('keydown', handleResetESCKeyDown);
+      };
+    },
+    [value, onReset],
+  );
+
+  return { ref, value, onChange, onReset };
 };
 
 export default useInput;
