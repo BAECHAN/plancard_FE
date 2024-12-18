b89eb5e298ce0ba7dcad102e70ddcf49634c3f19
BAECHAN <asdfjklddd@naver.com>
2024-12-18 22:45:50 +0900

    useKeyDown.tsx -> useKeydown.tsx 으로 변경

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,4 +1,4 @@
 export { default as useDebounce } from './useDebounce';
 export { default as useInput } from './useInput';
-export { default as useKeydown } from './useKeyDown';
+export { default as useKeydown } from './useKeydown';
 export { default as useToggle } from './useToggle';
