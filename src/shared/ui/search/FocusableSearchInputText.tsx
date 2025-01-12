6bc6061f2faa202dde73c50d985e0d53400a298d
BAECHAN <asdfjklddd@naver.com>
2025-01-12 18:59:03 +0900

    FocusableSearchInputText  memo로 감싸기

--- src/shared/ui/search/FocusableSearchInputText.tsx
+++ src/shared/ui/search/FocusableSearchInputText.tsx
@@ -2,30 +2,29 @@ import { useKeydown } from '@/shared/hooks';
 import SearchInputText, {
   SearchInputTextProps,
 } from '@/shared/ui/search/SearchInputText';
-import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { forwardRef, memo, useImperativeHandle, useRef } from 'react';
 
 /**
  * @description '/' 키를 누르면 검색 입력창에 포커스를 주는 Search widget
  */
-const FocusableSearchInputText = forwardRef<
-  HTMLInputElement,
-  SearchInputTextProps
->((props, ref) => {
-  const searchInputRef = useRef<HTMLInputElement>(null);
+const FocusableSearchInputText = memo(
+  forwardRef<HTMLInputElement, SearchInputTextProps>((props, ref) => {
+    const searchInputRef = useRef<HTMLInputElement>(null);
 
-  useKeydown('/', () => {
-    searchInputRef.current?.focus();
-  }); // '/' 키를 누르면 검색 입력창에 포커스
+    useKeydown('/', () => {
+      searchInputRef.current?.focus();
+    }); // '/' 키를 누르면 검색 입력창에 포커스
 
-  useImperativeHandle(ref, () => searchInputRef.current!); // 부모컴포넌트에서 ref로 검색 입력창에 접근
+    useImperativeHandle(ref, () => searchInputRef.current!); // 부모컴포넌트에서 ref로 검색 입력창에 접근
 
-  return (
-    <SearchInputText
-      ref={searchInputRef}
-      {...props}
-    />
-  );
-});
+    return (
+      <SearchInputText
+        ref={searchInputRef}
+        {...props}
+      />
+    );
+  }),
+);
 
 FocusableSearchInputText.displayName = 'FocusableSearchInputText';
 
