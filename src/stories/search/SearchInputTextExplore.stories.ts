855d01250aa849bab6f11015f6060bf8387d2ba2
BAECHAN <asdfjklddd@naver.com>
2025-01-12 19:08:02 +0900

    제거된 Props Storybook에 적용

--- src/stories/search/SearchInputTextExplore.stories.ts
+++ src/stories/search/SearchInputTextExplore.stories.ts
@@ -10,27 +10,13 @@ const meta: Meta<typeof SearchInputTextExplore> = {
   },
   tags: ['autodocs'],
   argTypes: {
-    defaultValue: {
-      control: 'text',
-      description: '검색 초기값 텍스트',
-    },
-
     placeholder: {
       control: 'text',
       description: '검색 텍스트 플레이스홀더',
     },
-    label: {
-      control: 'text',
-      description: '검색 텍스트 라벨',
-    },
     type: {
       control: 'text',
       description: '검색 텍스트 타입',
-    },
-    align: {
-      control: 'radio',
-      options: ['left', 'center', 'right'],
-      description: '검색 텍스트 정렬',
     },
   },
   args: {
@@ -44,7 +30,5 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {
-    defaultValue: '감사',
-  },
+  args: {},
 };
