86a240ff962eaf18ac41488dafe69f058adb09e8
BAECHAN <asdfjklddd@naver.com>
2025-01-12 19:01:17 +0900

    외부컴포넌트에서 Swiper를 맨앞으로 index를 보내는등의 핸들링을 할 수 있도록 추가

--- src/widgets/swiper/ui/CardImageSwiper.tsx
+++ src/widgets/swiper/ui/CardImageSwiper.tsx
@@ -11,15 +11,18 @@ import {
   ImageSwiperProps,
   StyledSwiper,
 } from '@/widgets/swiper/ui/ImageSwiper';
+import Swiper from 'swiper';
 import { Keyboard, Mousewheel, Navigation, Pagination } from 'swiper/modules';
 
 type CardImageSwiperProps = ImageSwiperProps & {
   onIndexChange?: (index: number) => void;
+  onSwiperReady?: (swiper: Swiper) => void;
 };
 
 const CardImageSwiper = ({
   imageList,
   onIndexChange,
+  onSwiperReady,
   size = 'medium',
 }: CardImageSwiperProps) => {
   return (
@@ -33,6 +36,9 @@ const CardImageSwiper = ({
       className="mySwiper"
       onSlideChange={swiper => {
         onIndexChange?.(swiper.activeIndex); // 현재 인덱스를 부모로 전달
+      }}
+      onSwiper={swiper => {
+        onSwiperReady?.(swiper);
       }}
     >
       {imageList.map((image, index) => {
