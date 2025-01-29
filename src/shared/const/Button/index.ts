8575ef26b3b53347aa021a8bb2f1d88b797624fd
BAECHAN <asdfjklddd@naver.com>
2025-01-29 19:08:22 +0900

    Button 컴포넌트 에 min-w를 추가하여 텍스트가 적어도 width를 넓히게 ESC Button에선 적용되지않게 수정

--- src/shared/const/Button/index.ts
+++ src/shared/const/Button/index.ts
@@ -1,8 +1,8 @@
-export const xSmall = 'px-2 h-5 text-[0.5rem]';
-export const small = 'px-3 py-1 h-7 text-xs';
-export const medium = 'px-4 py-1 h-9 text-sm';
-export const large = 'px-5 py-1 h-11 text-base';
-export const xLarge = 'px-6 py-1 h-14 text-xl';
+export const xSmall = ' h-5 text-[0.5rem] min-w-8';
+export const small = 'py-1 h-7 text-xs min-w-16';
+export const medium = 'py-1 h-9 text-sm min-w-24';
+export const large = 'py-1 h-11 text-base min-w-32';
+export const xLarge = 'py-1 h-14 text-xl min-w-40';
 
 export const primary = 'bg-primary text-white';
 export const gray = 'bg-gray text-white';
