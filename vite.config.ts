de4162dd63136574a6ee0a9dd95039aa70f91c5f
BAECHAN <asdfjklddd@naver.com>
2025-01-30 22:26:49 +0900

    github pages로 배포하는데 env파일에 작성해도 소용이 없어서 vite에 하드코딩

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
+
+// GitHub 레포지토리 이름을 여기에 직접 입력
+const REPOSITORY_NAME = 'plancard_FE';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), 'VITE_');
-
   return {
     plugins: [react()],
-    base: mode === 'production' ? `/${env.VITE_REPOSITORY_NAME}/` : '/',
+    base: mode === 'production' ? `/${REPOSITORY_NAME}/` : '/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
