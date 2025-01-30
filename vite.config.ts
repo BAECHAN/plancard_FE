746353379dc4be11c682354804699119ebba3e06
BAECHAN <asdfjklddd@naver.com>
2025-01-30 22:50:13 +0900

    vite 파일 재설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,12 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-// GitHub 레포지토리 이름을 여기에 직접 입력
-const REPOSITORY_NAME = 'plancard_FE';
-
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    base: mode === 'production' ? `/${REPOSITORY_NAME}/` : '/',
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
+export default defineConfig({
+  plugins: [react()],
+  base: '',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-  };
+  },
 });
