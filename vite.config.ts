90f28e0f7227084e370c2d47d637e6a3eba9a1b0
BAECHAN <asdfjklddd@naver.com>
2025-01-30 22:18:39 +0900

    env - local & prod 분리

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  plugins: [react()],
-  base:
-    process.env.NODE_ENV === 'production' ? `/${process.env.REPO_NAME}/` : '/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  // 환경 변수 로드
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_URL,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+  };
 });
