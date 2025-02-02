16cb33d8a996eb45ed3016652269212517bd1447
BAECHAN <asdfjklddd@naver.com>
2025-02-02 15:35:14 +0900

    빌드 시 PUBLIC_URL (CRA) 또는 base (Vite)를 올바르게 설정하여, 정적 자산의 경로가 https://username.github.io/레포지토리이름/를 포함 후
    GitHub Actions(main.yml) 파일 내 빌드 명령어에 환경 변수를 지정

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  base: process.env.VITE_BASE || '/', // GitHub Actions에서 환경 변수로 설정 가능
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
