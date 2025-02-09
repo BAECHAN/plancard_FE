13cc81aa7d66a3d001b9542e5aec9ef111360336
BAECHAN <asdfjklddd@naver.com>
2025-02-09 20:02:01 +0900

    CardPickPage 에서 BaseAlert 추가

--- src/pages/main/ui/CardPickPage.tsx
+++ src/pages/main/ui/CardPickPage.tsx
@@ -1,5 +1,5 @@
 import { MyOrExplore, usePickViewStore } from '@/shared/store';
-import { BaseButton } from '@/shared/ui';
+import { BaseAlert, BaseButton } from '@/shared/ui';
 import {
   ContentContainer,
   ControlContainer,
@@ -9,6 +9,7 @@ import {
   MainContainer,
 } from '@/widgets/layout/ui';
 import { useState } from 'react';
+import { PiWarningCircleFill } from 'react-icons/pi';
 
 export const CardPickPage = () => {
   const [type, setType] = useState<MyOrExplore>('my');
@@ -22,20 +23,33 @@ export const CardPickPage = () => {
   };
 
   return (
-    <MainContainer>
-      <ControlContainer>
-        <BaseButton onClick={handleCardPickCompleteClick}>
+    <>
+      <BaseAlert
+        variant="cream"
+        size="medium"
+      >
+        <PiWarningCircleFill className="text-2xl" />
+        <span>[ 플랜 ]에서 넘어온 [ 카드 선택 ] 화면입니다.</span>
+        <BaseButton
+          onClick={handleCardPickCompleteClick}
+          variant="primary"
+          size="small"
+        >
           카드 선택 완료
         </BaseButton>
-        {type === 'explore' ? (
-          <ControlContainerExplore />
-        ) : (
-          <ControlContainerMy />
-        )}
-      </ControlContainer>
-      <ContentContainer>
-        <ListContainerCard />
-      </ContentContainer>
-    </MainContainer>
+      </BaseAlert>
+      <MainContainer>
+        <ControlContainer>
+          {type === 'explore' ? (
+            <ControlContainerExplore />
+          ) : (
+            <ControlContainerMy />
+          )}
+        </ControlContainer>
+        <ContentContainer>
+          <ListContainerCard />
+        </ContentContainer>
+      </MainContainer>
+    </>
   );
 };
