08c05ecf77f72a66a5014e8b0a4f3c2cb6d3dcbf
BAECHAN <asdfjklddd@naver.com>
2025-02-16 15:59:55 +0900

    CardSwiper에 ESC버튼 추가

--- src/stories/widgets/swiper/CardSwiper.stories.tsx
+++ src/stories/widgets/swiper/CardSwiper.stories.tsx
@@ -23,5 +23,6 @@ type Story = StoryObj<typeof meta>;
 export const Large: Story = {
   args: {
     size: 'large',
+    onClose: () => {},
   },
 };
--- src/widgets/swiper/ui/CardSwiper.tsx
+++ src/widgets/swiper/ui/CardSwiper.tsx
@@ -10,7 +10,9 @@ import 'swiper/css/pagination';
 
 // import required modules
 import { Size } from '@/shared/type';
+import { ESCButton } from '@/shared/ui';
 import { Card } from '@/widgets/card/ui';
+import { Block } from '@/widgets/layout/ui';
 import { Keyboard, Mousewheel, Navigation, Pagination } from 'swiper/modules';
 
 const StyledSwiper = styled(Swiper)`
@@ -51,55 +53,69 @@ const StyledSwiper = styled(Swiper)`
     background: #3498db; /* 원하는 색상으로 변경 */
   }
 `;
-const CardSwiper = ({ size = 'medium' }: { size: Size }) => {
+const CardSwiper = ({
+  size = 'medium',
+  onClose,
+}: {
+  size: Size;
+  onClose: () => void;
+}) => {
   return (
-    <StyledSwiper
-      slidesPerView={5}
-      centeredSlides={false}
-      spaceBetween={15}
-      grabCursor
-      navigation
-      pagination={{
-        clickable: true,
-      }}
-      modules={[Pagination, Keyboard, Mousewheel, Navigation]}
-      className="mySwiper"
-    >
-      {Array.from({ length: 10 }).map((_, index) => {
-        return (
-          <SwiperSlide key={index}>
-            <Card
-              showDelete
-              onDelete={() => {}}
-              size="small"
-              info={{
-                cardId: '1',
-                theme: ['symbol', 'night'],
-                title: '에펠탑',
-                description:
-                  '프랑스 파리 안나톨 5가(5 Av.Anatole)에 있는 탑이다. 탑의 이름은 건축가 에펠의 이름을 딴것으로 1889년 3월 31일 준공해 1889년 5월 6일에 개장했다. 프랑스의 건축가 알렉상드르 귀스타브 에펠(Alexandre Gustave Eiffel, 1832~1923)[6]이 만든 거대한 철탑.',
-                country: 'France',
-                city: 'Paris',
-                scrap: false,
-                imageList: [
-                  {
-                    imageId: '1',
-                    imageUrl: `${import.meta.env.BASE_URL}images/eiffel-tower.svg`,
-                    alt: '에펠탑',
-                    isMain: true,
-                  },
-                ],
-                rating: 4,
-                category: '관광지',
-                googleMapLink:
-                  'https://www.google.com/maps/place/%EC%97%90%ED%8E%A0%ED%83%91/data=!4m2!3m1!1s0x47e66e2964e34e2d:0x8ddca9ee380ef7e0?sa=X&ved=1t:155783&ictx=111',
-              }}
-              onClick={() => {}}
-            />
-          </SwiperSlide>
-        );
-      })}
-    </StyledSwiper>
+    <div className="fixed left-1/2 z-50 -translate-x-1/2">
+      <Block
+        label="card-swiper"
+        className="relative right-3 top-11 z-50 flex justify-end"
+      >
+        <ESCButton onClick={onClose} />
+      </Block>
+      <StyledSwiper
+        slidesPerView={5}
+        centeredSlides={false}
+        spaceBetween={15}
+        grabCursor
+        navigation
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination, Keyboard, Mousewheel, Navigation]}
+        className="mySwiper"
+      >
+        {Array.from({ length: 10 }).map((_, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <Card
+                showDelete
+                onDelete={() => {}}
+                size="small"
+                info={{
+                  cardId: '1',
+                  theme: ['symbol', 'night'],
+                  title: '에펠탑',
+                  description:
+                    '프랑스 파리 안나톨 5가(5 Av.Anatole)에 있는 탑이다. 탑의 이름은 건축가 에펠의 이름을 딴것으로 1889년 3월 31일 준공해 1889년 5월 6일에 개장했다. 프랑스의 건축가 알렉상드르 귀스타브 에펠(Alexandre Gustave Eiffel, 1832~1923)[6]이 만든 거대한 철탑.',
+                  country: 'France',
+                  city: 'Paris',
+                  scrap: false,
+                  imageList: [
+                    {
+                      imageId: '1',
+                      imageUrl: `${import.meta.env.BASE_URL}images/eiffel-tower.svg`,
+                      alt: '에펠탑',
+                      isMain: true,
+                    },
+                  ],
+                  rating: 4,
+                  category: '관광지',
+                  googleMapLink:
+                    'https://www.google.com/maps/place/%EC%97%90%ED%8E%A0%ED%83%91/data=!4m2!3m1!1s0x47e66e2964e34e2d:0x8ddca9ee380ef7e0?sa=X&ved=1t:155783&ictx=111',
+                }}
+                onClick={() => {}}
+              />
+            </SwiperSlide>
+          );
+        })}
+      </StyledSwiper>
+    </div>
   );
 };
 
