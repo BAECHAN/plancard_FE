429c958706418e745e59efbbb94b1ac34e98b8c0
BAECHAN <asdfjklddd@naver.com>
2025-03-23 21:58:43 +0900

    테스트 파일 제거

--- src/widgets/layout/ui/UserEditFormLayout.tsx
+++
@@ -1,6 +0,0 @@
-import { UserEditForm } from '@/features/user/ui';
-
-const UserEditFormLayout = (props: { onSave: () => void }) => {
-  return <UserEditForm {...props} />;
-};
-export default UserEditFormLayout;
